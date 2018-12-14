8b20f6f8e0974f7a8b146fc87b0a3ab5b0420697
liangzhicheng <liangzhicheng@meizu.com>
2018-12-14 10:16:10 +0800

    修改跳转地址

--- src/components/page/JsonView.vue
+++ src/components/page/JsonView.vue
@@ -2,7 +2,7 @@
     <div id="wrapper">
         <el-form ref="json" label-width="80px">
             <el-form-item label="">
-                <iframe src="https://www.bejson.com/jsoneditoronline/" vspace="-200" hspace="-800"
+                <iframe src="http://www.bejson.com/jsoneditoronline/" vspace="-200" hspace="-800"
                         scrolling="yes" height="850px" width="100%" frameborder="0"></iframe>
             </el-form-item>
         </el-form>
